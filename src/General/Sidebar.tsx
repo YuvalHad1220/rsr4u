1ec9b291d75a67e3d401c2847fb18a79453c9e9a
Yuval Hadar <35yuval2@gmail.com>
2023-09-21 20:13:13 +0300

    cleared navbar, may need modifications in the future

--- src/General/Sidebar.tsx
+++ src/General/Sidebar.tsx
@@ -2,15 +2,12 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 
 import BugReportIcon from '@mui/icons-material/BugReport';
 import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
@@ -20,13 +17,12 @@ import TableChartIcon from '@mui/icons-material/TableChart';
 import UpdateIcon from '@mui/icons-material/Update';
 import LogoutIcon from '@mui/icons-material/Logout';
 import InfoIcon from '@mui/icons-material/Info';
-import { useState } from 'react';
+import { Typography } from '@mui/material';
 
 const drawerWidth = "20rem";
 
 
 const Sidebar = () => {
-    const [mobileOpen, setMobileOpen] = useState(false);
 
     const adminOptions = [{text: "דיבאגר", icon: <BugReportIcon color='secondary'/>}];
 
@@ -40,10 +36,6 @@ const Sidebar = () => {
 
     const optionsList = [adminOptions, rasarAsisstantOptions, rasarWorkerOptions, rasarOptions, loggedInOptions]; // will be determined by permissions system
 
-    const handleDrawerToggle = () => {
-        setMobileOpen(!mobileOpen);
-      };
-
     const OptionsListComp = optionsList.map((options, i) => {
         return (
         <Box key={i}>
@@ -55,7 +47,7 @@ const Sidebar = () => {
                     <ListItemIcon>
                         {option.icon}
                     </ListItemIcon>
-                    <ListItemText primary={option.text} />
+                    <ListItemText primary={option.text}/>
                 </ListItemButton>
             </ListItem>
             )
@@ -63,80 +55,18 @@ const Sidebar = () => {
         </Box>
     )});
   
-  
-    const drawer = (
-        <>
-            <Toolbar />
-            <List>
-                {OptionsListComp}
-            </List>
-        </>
-
-    );
-
 
     return (
-        <Box sx={{ display: 'flex' }}>
-          <AppBar
-            position="fixed"
-            sx={{
-              width: { sm: `calc(100% - ${drawerWidth}px)` },
-              ml: { sm: `${drawerWidth}px` },
-            }}
-          >
-            <Toolbar>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                edge="start"
-                onClick={handleDrawerToggle}
-                sx={{ mr: 2, display: { sm: 'none' } }}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" noWrap component="div">
-                Responsive drawer
-              </Typography>
-            </Toolbar>
-          </AppBar>
           <Box
             component="nav"
-            sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-            aria-label="mailbox folders"
-          >
-            {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-            <Drawer
-              variant="temporary"
-              open={mobileOpen}
-              onClose={handleDrawerToggle}
-              ModalProps={{
-                keepMounted: true, // Better open performance on mobile.
-              }}
-              sx={{
-                display: { xs: 'block', sm: 'none' },
-                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-              }}
-            >
-              {drawer}
+            aria-label="סרגל ניווט ראשי" >
+            <Drawer variant="permanent" >
+                <Toolbar />
+                <List>
+                    {OptionsListComp}
+                </List>
             </Drawer>
-            <Drawer
-              variant="permanent"
-              sx={{
-                display: { xs: 'none', sm: 'block' },
-                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-              }}
-              open
-            >
-              {drawer}
-            </Drawer>
-          </Box>
-          <Box
-            component="main"
-            sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
-          >
-            <Toolbar />
-          </Box>
-        </Box>
+          </Box>          
       );
 }
 
